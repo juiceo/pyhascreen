ff2d8e86669ac410e3b7cb162f3383134d446640
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2018-12-21 15:06:11 +0200

    Updates

---
+++ precache-manifest.3d77b3212df05b3a4dd9fc1406d5007e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pyhascreen/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8bb6c8fadb6a0977c5aa",
+    "url": "/pyhascreen/static/js/runtime~main.8bb6c8fa.js"
+  },
+  {
+    "revision": "8731d4f22924789b4868",
+    "url": "/pyhascreen/static/js/main.8731d4f2.chunk.js"
+  },
+  {
+    "revision": "e09c6691b3de2f80e710",
+    "url": "/pyhascreen/static/js/1.e09c6691.chunk.js"
+  },
+  {
+    "revision": "8731d4f22924789b4868",
+    "url": "/pyhascreen/static/css/main.f8f59209.chunk.css"
+  },
+  {
+    "revision": "68c69de2a71aa5741bb59f3f5f1b3797",
+    "url": "/pyhascreen/index.html"
+  }
+];
